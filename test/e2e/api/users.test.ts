2ce4779921ed44b2d4489a7cbd7c71ea62473d16
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-04-20 15:27:23 +0200

    :tada: Initial commit

--- test/e2e/api/users.test.ts
+++ test/e2e/api/users.test.ts
@@ -5,10 +5,10 @@ import { User } from '../../../src/api/models/User';
 import { CreateBruce } from '../../../src/database/seeds/CreateBruce';
 import { runSeed } from '../../../src/lib/seed';
 import { closeDatabase } from '../../utils/database';
-import { fakeAuthenticationForUser } from '../utils/auth';
 import { BootstrapSettings } from '../utils/bootstrap';
 import { prepareServer } from '../utils/server';
 
+// import { fakeAuthenticationForUser } from '../utils/auth';
 describe('/api/users', () => {
 
     let bruce: User;
@@ -21,7 +21,7 @@ describe('/api/users', () => {
     beforeAll(async () => {
         settings = await prepareServer({ migrate: true });
         bruce = await runSeed<User>(CreateBruce);
-        fakeAuthenticationForUser(bruce, true);
+        // fakeAuthenticationForUser(bruce, true);
     });
 
     // -------------------------------------------------------------------------
@@ -40,7 +40,8 @@ describe('/api/users', () => {
     test('GET: / should return a list of users', async (done) => {
         const response = await request(settings.app)
             .get('/api/users')
-            .set('Authorization', `Bearer 1234`)
+            // .set('Authorization', `Bearer 1234`)
+            .set('Authorization', `Basic ${bruce.toBase64()}`)
             .expect('Content-Type', /json/)
             .expect(200);
 
@@ -51,13 +52,15 @@ describe('/api/users', () => {
     test('GET: /:id should return bruce', async (done) => {
         const response = await request(settings.app)
             .get(`/api/users/${bruce.id}`)
-            .set('Authorization', `Bearer 1234`)
+            // .set('Authorization', `Bearer 1234`)
+            .set('Authorization', `Basic ${bruce.toBase64()}`)
             .expect('Content-Type', /json/)
             .expect(200);
 
         expect(response.body.id).toBe(bruce.id);
         expect(response.body.firstName).toBe(bruce.firstName);
         expect(response.body.lastName).toBe(bruce.lastName);
+        expect(response.body.username).toBe(bruce.username);
         expect(response.body.email).toBe(bruce.email);
         done();
     });
